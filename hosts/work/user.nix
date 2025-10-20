1e2077fd304106a7bf6c7e8a88bd4ce7813a764b
aleksandr.simonov <aleksandr.simonov@exness.com>
2025-10-20 11:01:05 +0300

    work fixes

--- hosts/work/user.nix
+++ hosts/work/user.nix
@@ -4,25 +4,22 @@
     home = "/Users/aleksandr.simonov";
   };
 
-  home-manager.users.alex = {
+  home-manager.users."aleksandr.simonov" = {
     home.stateVersion = "25.11";
 
-    home-manager.users.alex = {
-      home.stateVersion = "25.11";
+    imports = [
+      ../../modules/common/apps/other.nix
 
-      imports = [
-        ../../modules/common/apps/other.nix
+      ../../modules/common/shell
 
-        ../../modules/common/shell
-
-        ../../modules/common/apps/bat.nix
-        ../../modules/common/apps/btop.nix
-        ../../modules/common/apps/fzf.nix
-        ../../modules/common/apps/delta.nix
-        ../../modules/common/apps/git/git.homeMac.nix
-        ../../modules/common/apps/alacritty.nix
-        ../../modules/common/apps/zed.nix
-      ];
-    };
+      ../../modules/common/apps/bat.nix
+      ../../modules/common/apps/btop.nix
+      ../../modules/common/apps/fzf.nix
+      ../../modules/common/apps/delta.nix
+      ../../modules/common/apps/git/git.work.nix
+      ../../modules/common/apps/alacritty.nix
+      ../../modules/common/apps/zed.nix
+      ../../modules/common/apps/k9s.nix
+    ];
   };
 }
--- modules/common/apps/k9s.nix
+++ modules/common/apps/k9s.nix
@@ -1,5 +1,40 @@
 {...}: {
   programs.k9s = {
-    enabled = true;
+    enable = true;
+
+    settings = {
+      k9s = {
+        refreshRate = 2;
+        maxConnRetry = 5;
+        readOnly = false;
+        noExitOnCtrlC = false;
+        skipLatestRevCheck = false;
+        disablePodCounting = false;
+        ui = {
+          skin = "nord";
+          enableMouse = false;
+          headless = false;
+          logoless = true;
+          crumbsless = false;
+          reactive = true;
+          noIcons = true;
+        };
+      };
+    };
+
+    aliases = {
+      dp = "deployments";
+      sec = "v1/secrets";
+      jo = "jobs";
+      cr = "clusterroles";
+      crb = "clusterrolebindings";
+      ro = "roles";
+      rb = "rolebindings";
+      np = "networkpolicies";
+    };
+
+    skins = {
+      nord = ./k9s.nord.yaml;
+    };
   };
 }
