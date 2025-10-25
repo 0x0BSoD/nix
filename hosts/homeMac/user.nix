5b3a4b5955d8b7a018c91e6b51c153995b72b815
0x0BSoD <zlodey23@gmail.com>
2025-10-25 17:17:15 +0300

    home: updates

--- hosts/homeMac/user.nix
+++ hosts/homeMac/user.nix
@@ -5,7 +5,31 @@
   };
 
   home-manager.users.alex = {
-    home.stateVersion = "25.11";
+    home = {
+      stateVersion = "25.11";
+
+      sessionVariables = {
+        EDITOR = "vim";
+        PAGER = "";
+
+        GOPATH = "$HOME/Projects/go";
+        GO111MODULE = "on";
+
+        FZF_CTRL_R_OPTS = "
+       --preview 'echo {}' --preview-window up:3:hidden:wrap
+       --bind 'ctrl-/:toggle-preview'
+       --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+       --color header:italic
+       --header 'Press CTRL-Y to copy command into clipboard'";
+        FZF_CTRL_T_OPTS = "
+         --walker-skip .git,node_modules,target
+         --preview 'bat -n --color=always {}'
+         --bind 'ctrl-/:change-preview-window(down|hidden|)'";
+        FZF_ALT_C_OPTS = "
+           --walker-skip .git,node_modules,target
+           --preview 'tree -C {}'";
+      };
+    };
 
     imports = [
       ../../modules/common/apps/other.nix
