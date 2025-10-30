4140efb298f67a9bc199f5785761c387bd949941
0x0BSoD <zlodey23@gmail.com>
2025-10-30 23:54:41 +0200

    home: waybar conf init

--- modules/linux/apps/hyprland/waybar/settings.nix
+++ modules/linux/apps/hyprland/waybar/settings.nix
@@ -2,111 +2,62 @@
   programs.waybar.settings.mainBar = {
     position = "top";
     layer = "top";
-    # height = 28;
 
-    margin-top = 0;
-    margin-bottom = 0;
-    margin-left = 0;
-    margin-right = 0;
+    margin-bottom = -10;
 
     modules-left = [
       "hyprland/workspaces"
+      "cpu"
     ];
+
     modules-center = [
-      "custom/media"
-    ];
-    modules-right = [
-      "hyprland/language"
-      "custom/notifications"
-      "tray"
       "clock"
     ];
 
-    clock = {
-      calendar = {
-        format = {
-          today = "<span color='#98971A'><b>{}</b></span>";
-        };
-      };
-      format = "  {:%H:%M}";
-      tooltip = "true";
-      tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-      format-alt = "  {:%d/%m}";
-    };
+    modules-right = [
+      "bluetooth"
+      "network"
+      "pulseaudio"
+    ];
+
+    #================================
 
     "hyprland/workspaces" = {
-      active-only = false;
-      disable-scroll = true;
-      format = "{icon}";
-      on-click = "activate";
+      format = "{name}: {icon}";
       format-icons = {
-        "1" = "I";
-        "2" = "II";
-        "3" = "III";
-        "4" = "IV";
-        "5" = "V";
-        "6" = "VI";
-        "7" = "VII";
-        "8" = "VIII";
-        "9" = "IX";
-        "10" = "X";
-        sort-by-number = true;
-      };
-      persistent-workspaces = {
-        "1" = [];
-        "2" = [];
-        "3" = [];
-        "4" = [];
-        "5" = [];
+        active = "";
+        default = "";
       };
     };
 
-    tray = {
-      icon-size = 20;
-      spacing = 8;
+    network = {
+      format-wifi = "󰤢";
+      format-ethernet = "󰈀 ";
+      format-disconnected = "󰤠 ";
+      interval = 5;
+      tooltip-format = "{essid} ({signalStrength}%)";
+      on-click = "nm-connection-editor";
     };
 
-    "hyprland/language" = {
-      tooltip = true;
-      tooltip-format = "Keyboard layout";
-      format = "<span foreground='#FABD2F'> </span> {}";
-      format-ru = "RU";
-      format-en = "US";
-      on-click = "hyprctl switchxkblayout at-translated-set-2-keyboard next";
+    cpu = {
+      interval = 1;
+      format = "  {icon0}{icon1}{icon2}{icon3} {usage:>2}%";
+      format-icons = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+      on-click = "ghostty -e htop";
     };
 
-    "custom/notification" = {
-      tooltip = true;
-      tooltip-format = "Notifications";
-      format = "{icon}";
+    bluetooth = {
+      format = "󰂲";
+      format-on = "{icon}";
+      format-off = "{icon}";
+      format-connected = "{icon}";
       format-icons = {
-        notification = "<span foreground='red'><sup></sup></span>";
-        none = "";
-        dnd-notification = "<span foreground='red'><sup></sup></span>";
-        dnd-none = "";
-        inhibited-notification = "<span foreground='red'><sup></sup></span>";
-        inhibited-none = "";
-        dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
-        dnd-inhibited-none = "";
+        on = "󰂯";
+        off = "󰂲";
+        connected = "󰂱";
       };
-      return-type = "json";
-      exec-if = "which swaync-client";
-      exec = "swaync-client -swb";
-      on-click = "swaync-client -t -sw";
-      on-click-right = "swaync-client -d -sw";
-      escape = true;
-    };
-
-    "custom/media" = {
-      format = "{icon} {}";
-      return-type = "json";
-      max-length = 40;
-      format-icons = {
-        spotify = "";
-        default = "🎜";
-      };
-      escape = true;
-      exec = "$HOME/.config/waybar/mediaplayer.py 2> /dev/null";
+      on-click = "blueman-manager";
+      tooltip-format-connected = "{device_enumerate}";
     };
   };
 }
