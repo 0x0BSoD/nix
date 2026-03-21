27258485eccc5432d00182a0e5290ae22eed8548
aleksandr.simonov <aleksandr.simonov@exness.com>
2026-03-21 19:37:06 +0200

    ops, path fix

--- hosts/darwin/work/user.nix
+++ hosts/darwin/work/user.nix
@@ -23,37 +23,37 @@ in {
       inputs.zen-browser.homeModules.beta
       inputs.spicetify-nix.homeManagerModules.spicetify
 
-      ./${appsPath}/git/git.work.nix
+      "${appsPath}/git/git.work.nix"
 
-      ./${appsPath}/develop/nixdev.nix
-      ./${appsPath}/develop/go.nix
-      ./${appsPath}/develop/java.nix
-      ./${appsPath}/develop/rust.nix
-      ./${appsPath}/develop/python.nix
-      ./${appsPath}/develop/nodejs.nix
-      ./${appsPath}/develop/tools.nix
+      "${appsPath}/develop/nixdev.nix"
+      "${appsPath}/develop/go.nix"
+      "${appsPath}/develop/java.nix"
+      "${appsPath}/develop/rust.nix"
+      "${appsPath}/develop/python.nix"
+      "${appsPath}/develop/nodejs.nix"
+      "${appsPath}/develop/tools.nix"
 
-      ./${appsPath}/kubernetes/k9s
-      ./${appsPath}/kubernetes/kubectl.nix
-      ./${appsPath}/kubernetes/kubecm.nix
-      ./${appsPath}/kubernetes/kubectx.nix
-      ./${appsPath}/kubernetes/minikube.nix
-      ./${appsPath}/kubernetes/k3s.nix
-      ./${appsPath}/kubernetes/stern.nix
+      "${appsPath}/kubernetes/k9s"
+      "${appsPath}/kubernetes/kubectl.nix"
+      "${appsPath}/kubernetes/kubecm.nix"
+      "${appsPath}/kubernetes/kubectx.nix"
+      "${appsPath}/kubernetes/minikube.nix"
+      "${appsPath}/kubernetes/k3s.nix"
+      "${appsPath}/kubernetes/stern.nix"
 
-      ./${appsPath}/bat.nix
-      ./${appsPath}/btop.nix
-      ./${appsPath}/delta.nix
-      # ./${appsPath}/terraform.nix
-      ./${appsPath}/vault.nix
-      ./${appsPath}/redis.nix
-      ./${appsPath}/sysdig.nix
-      ./${appsPath}/ghostty
-      ./${appsPath}/shell
+      "${appsPath}/bat.nix"
+      "${appsPath}/btop.nix"
+      "${appsPath}/delta.nix"
+      # "${appsPath}/terraform.nix"
+      "${appsPath}/vault.nix"
+      "${appsPath}/redis.nix"
+      "${appsPath}/sysdig.nix"
+      "${appsPath}/ghostty"
+      "${appsPath}/shell"
 
-      ./${appsPath}/zed.nix
-      ./${appsPath}/zen-browser.nix
-      ./${appsPath}/spicetify.nix
+      "${appsPath}/zed.nix"
+      "${appsPath}/zen-browser.nix"
+      "${appsPath}/spicetify.nix"
 
       "${appsPath}/session-vars.nix"
     ];
--- hosts/linux/tpad/user.nix
+++ hosts/linux/tpad/user.nix
@@ -11,115 +11,114 @@ in {
   programs.dconf.enable = true;
 
   home-manager.users."${primaryUser}" = {
-      programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
 
-      services.kanshi = {
-        enable = true;
+    services.kanshi = {
+      enable = true;
 
-        settings = [
-          {
-            profile.name = "undocked";
-            profile.outputs = [
-              {
-                criteria = "eDP-1";
-                status = "enable";
-                mode = "1920x1080@60Hz";
-                scale = 1.0;
-                position = "0,0";
-              }
-            ];
-          }
+      settings = [
+        {
+          profile.name = "undocked";
+          profile.outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              mode = "1920x1080@60Hz";
+              scale = 1.0;
+              position = "0,0";
+            }
+          ];
+        }
 
-          {
-            profile.name = "docked";
-            profile.outputs = [
-              {
-                criteria = "eDP-1";
-                status = "disable";
-              }
-              {
-                criteria = "HDMI-A-1";
-                status = "enable";
-                mode = "2560x1080@180.00Hz";
-                position = "0,0";
-              }
-            ];
-          }
-        ];
-      };
+        {
+          profile.name = "docked";
+          profile.outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "HDMI-A-1";
+              status = "enable";
+              mode = "2560x1080@180.00Hz";
+              position = "0,0";
+            }
+          ];
+        }
+      ];
+    };
 
-      home = {
-        stateVersion = "25.11";
-        username = primaryUser;
-        homeDirectory = "/home/${primaryUser}";
-        sessionPath = [
-          "$HOME/go/bin"
-          "$HOME/.local/bin"
-        ];
-      };
+    home = {
+      stateVersion = "25.11";
+      username = primaryUser;
+      homeDirectory = "/home/${primaryUser}";
+      sessionPath = [
+        "$HOME/go/bin"
+        "$HOME/.local/bin"
+      ];
+    };
 
-      xdg = {
-        enable = true;
-        desktopEntries = {
-          nemo = {
-            name = "Nemo";
-            genericName = "File manager";
-            exec = "nemo %U";
-            icon = "nemo";
-            terminal = false;
-            categories = ["System" "FileManager"];
-          };
+    xdg = {
+      enable = true;
+      desktopEntries = {
+        nemo = {
+          name = "Nemo";
+          genericName = "File manager";
+          exec = "nemo %U";
+          icon = "nemo";
+          terminal = false;
+          categories = ["System" "FileManager"];
         };
       };
+    };
 
-      imports = [
-        inputs.zen-browser.homeModules.beta
-        inputs.spicetify-nix.homeManagerModules.spicetify
-        inputs.nixvim.homeModules.nixvim
+    imports = [
+      inputs.zen-browser.homeModules.beta
+      inputs.spicetify-nix.homeManagerModules.spicetify
+      inputs.nixvim.homeModules.nixvim
 
-        ## TODO: make config common for instances
-        ./${appsPath}/git/git.home.nix
+      "${appsPath}/git/git.home.nix"
 
-        ./${appsPath}/develop/nixdev.nix
-        ./${appsPath}/develop/go.nix
-        ./${appsPath}/develop/rust.nix
-        ./${appsPath}/develop/python.nix
-        ./${appsPath}/develop/nodejs.nix
-        ./${appsPath}/develop/tools.nix
+      "${appsPath}/develop/nixdev.nix"
+      "${appsPath}/develop/go.nix"
+      "${appsPath}/develop/rust.nix"
+      "${appsPath}/develop/python.nix"
+      "${appsPath}/develop/nodejs.nix"
+      "${appsPath}/develop/tools.nix"
 
-        ./${appsPath}/kubernetes/k9s
-        ./${appsPath}/kubernetes/kubectl.nix
-        ./${appsPath}/kubernetes/kubecm.nix
-        ./${appsPath}/kubernetes/kubectx.nix
-        ./${appsPath}/kubernetes/stern.nix
+      "${appsPath}/kubernetes/k9s"
+      "${appsPath}/kubernetes/kubectl.nix"
+      "${appsPath}/kubernetes/kubecm.nix"
+      "${appsPath}/kubernetes/kubectx.nix"
+      "${appsPath}/kubernetes/stern.nix"
 
-        ./${appsPath}/bat.nix
-        ./${appsPath}/btop.nix
-        ./${appsPath}/docker.nix
-        ./${appsPath}/delta.nix
-        ./${appsPath}/neovim.nix
-        ./${appsPath}/obsidian.nix
-        ./${appsPath}/telegram.nix
-        ./${appsPath}/ghostty
-        ./${appsPath}/shell
+      "${appsPath}/bat.nix"
+      "${appsPath}/btop.nix"
+      "${appsPath}/docker.nix"
+      "${appsPath}/delta.nix"
+      "${appsPath}/neovim.nix"
+      "${appsPath}/obsidian.nix"
+      "${appsPath}/telegram.nix"
+      "${appsPath}/ghostty"
+      "${appsPath}/shell"
 
-        ./${appsPath}/zed.nix
-        ./${appsPath}/goland.nix
-        ./${appsPath}/zen-browser.nix
-        ./${appsPath}/spicetify.nix
+      "${appsPath}/zed.nix"
+      "${appsPath}/goland.nix"
+      "${appsPath}/zen-browser.nix"
+      "${appsPath}/spicetify.nix"
 
-        ./${linuxPath}/appearance.nix
-        ./${linuxPath}/gnome.nix
-        ./${linuxPath}/hyprland
-        ./${linuxPath}/nemo.nix
-        ./${linuxPath}/other.nix
-        ./${linuxPath}/swaylock.nix
-        ./${linuxPath}/swayosd.nix
-        ./${linuxPath}/waypaper.nix
+      "${linuxPath}/appearance.nix"
+      "${linuxPath}/gnome.nix"
+      "${linuxPath}/hyprland"
+      "${linuxPath}/nemo.nix"
+      "${linuxPath}/other.nix"
+      "${linuxPath}/swaylock.nix"
+      "${linuxPath}/swayosd.nix"
+      "${linuxPath}/waypaper.nix"
 
-        "${appsPath}/session-vars.nix"
-        "${linuxPath}/scripts"
-      ];
+      "${appsPath}/session-vars.nix"
+      "${linuxPath}/scripts"
+    ];
   };
 
   users.users."${primaryUser}" = {
