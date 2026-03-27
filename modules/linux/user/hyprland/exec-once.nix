92fba3e91ce8d39cd2e914574299c1e79182fb9c
0x0BSoD <zlodey23@gmail.com>
2026-03-28 01:14:18 +0200

    updates

--- modules/linux/user/hyprland/exec-once.nix
+++ modules/linux/user/hyprland/exec-once.nix
@@ -2,14 +2,14 @@
   wayland.windowManager.hyprland.settings.exec-once = [
     "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
     "hyperidle &"
-    "waypaper --restore"
     "nm-applet &"
     "poweralertd &"
+    "quickshell -p /home/alex/.config/quickshell/shell.qml &"
     "swaync &"
     "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
     "udiskie --automount --notify --smart-tray &"
+    "waypaper --restore"
     "wl-clip-persist --clipboard both &"
     "wl-paste --watch cliphist store &"
-    "quickshell &"
   ];
 }
